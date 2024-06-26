ee765e4e29f4309c90ab3b69e73b4da64febbba2
SHASHANK SHUKLA <shashankshukla69204@gmail.com>
2024-06-26 17:05:36 +0530

    Update inline.py

--- StringGen/utils/inline.py
+++ StringGen/utils/inline.py
@@ -5,11 +5,11 @@ from config import SUPPORT_CHAT
 
 keyboard = InlineKeyboardMarkup(
     [
-        [InlineKeyboardButton(text="ɢᴇɴᴇʀᴀᴛᴇ sᴇssɪᴏɴ", callback_data="gensession")],
+        [InlineKeyboardButton(text="▪️ɢᴇɴᴇʀᴀᴛᴇ sᴇssɪᴏɴ▪️", callback_data="gensession")],
         [
-            InlineKeyboardButton(text="ᴜᴘᴅᴀᴛᴇs", url=SUPPORT_CHAT),
+            InlineKeyboardButton(text="▪️ᴜᴘᴅᴀᴛᴇs▪️", url=SUPPORT_CHAT),
             InlineKeyboardButton(
-                text="sᴜᴘᴘᴏʀᴛ", url="https://t.me/mastiwithfriendsxd"
+                text="▪️sᴜᴘᴘᴏʀᴛ▪️", url="https://t.me/mastiwithfriendsxd"
             ),
         ],
     ]
@@ -18,13 +18,13 @@ keyboard = InlineKeyboardMarkup(
 gen_key = InlineKeyboardMarkup(
     [
         [
-            InlineKeyboardButton(text="ᴩʏʀᴏɢʀᴀᴍ v1", callback_data="pyrogram1"),
-            InlineKeyboardButton(text="ᴩʏʀᴏɢʀᴀᴍ v2", callback_data="pyrogram"),
+            InlineKeyboardButton(text="🔹ᴩʏʀᴏɢʀᴀᴍ v1🔹", callback_data="pyrogram1"),
+            InlineKeyboardButton(text="🔸ᴩʏʀᴏɢʀᴀᴍ v2🔸", callback_data="pyrogram"),
         ],
-        [InlineKeyboardButton(text="ᴛᴇʟᴇᴛʜᴏɴ", callback_data="telethon")],
+        [InlineKeyboardButton(text="▪️ᴛᴇʟᴇᴛʜᴏɴ▪️", callback_data="telethon")],
     ]
 )
 
 retry_key = InlineKeyboardMarkup(
-    [[InlineKeyboardButton(text="ᴛʀʏ ᴀɢᴀɪɴ", callback_data="gensession")]]
+    [[InlineKeyboardButton(text="🔸ᴛʀʏ ᴀɢᴀɪɴ🔸", callback_data="gensession")]]
 )
